cb03648b7630fd9f2646640bb1dfcc7945ea090f
matyasbejo <bejomatyi@gmail.com>
2024-09-20 00:52:17 +0200

    Added base models project with one class

---
+++ InfoDroplets.Models/DropletDataRow.cs
@@ -0,0 +1,24 @@
+﻿namespace InfoDroplets.Models
+{
+    public class DropletDataRow
+    {
+        #region variables
+        private readonly int dropletId;
+        private readonly int satelliteCount;
+        private readonly double longitude;
+        private readonly double latitude;
+        private readonly double height;
+        private readonly DateTime time;
+        #endregion
+
+        public DropletDataRow(int dropletId, int satelliteCount, double longitude, double latitude, double height, DateTime time)
+        {
+            this.dropletId = dropletId;
+            this.satelliteCount = satelliteCount;
+            this.longitude = longitude;
+            this.latitude = latitude;
+            this.height = height;
+            this.time = time;
+        }
+    }
+}
