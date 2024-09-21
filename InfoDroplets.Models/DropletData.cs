d01cde8a5fdd72bc1901d29c94e2c56c73c24aef
matyasbejo <bejomatyi@gmail.com>
2024-09-21 15:20:50 +0200

    variables changed to properties with db constraints

--- InfoDroplets.Models/DropletData.cs
+++ InfoDroplets.Models/DropletData.cs
@@ -1,24 +1,46 @@
-﻿namespace InfoDroplets.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace InfoDroplets.Models
 {
     public class DropletData
     {
-        #region variables
-        private readonly int dropletId;
-        private readonly int satelliteCount;
-        private readonly double longitude;
-        private readonly double latitude;
-        private readonly double height;
-        private readonly DateTime time;
+        #region properties
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int DropletId { get; private set; }
+
+        [Required]
+        [Range(0, 32)]
+        public int SatelliteCount { get; private set; }
+
+        [Required]
+        [Range(0, 180)]
+        public double Longitude { get; private set; }
+
+        [Required]
+        [Range(0, 180)]
+        public double Latitude { get; private set; }
+
+        [Required]
+        [Range(0, 50000)]
+        public double Elevation { get; private set; }
+
+        [Required]
+        public DateTime Time { get; private set; }
         #endregion
 
-        public DropletData(int dropletId, int satelliteCount, double longitude, double latitude, double height, DateTime time)
+        public DropletData(int dropletId, int satelliteCount, double longitude, double latitude, double elevation, DateTime time)
         {
-            this.dropletId = dropletId;
-            this.satelliteCount = satelliteCount;
-            this.longitude = longitude;
-            this.latitude = latitude;
-            this.height = height;
-            this.time = time;
+            DropletId = dropletId;
+            SatelliteCount = satelliteCount;
+            Longitude = longitude;
+            Latitude = latitude;
+            Elevation = elevation;
+            Time = time;
         }
     }
 }
