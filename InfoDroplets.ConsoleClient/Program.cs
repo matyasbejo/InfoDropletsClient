b24d5562347e65362ce3a1c6950aff012ff994cc
matyasbejo <bejomatyi@gmail.com>
2024-09-21 13:12:05 +0200

    Added new repo project a a console client for testing

---
+++ InfoDroplets.ConsoleClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using InfoDroplets.Models;
+
+namespace InfoDroplets.ConsoleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ InfoDroplets.Repository/DropletDataDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using InfoDroplets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfoDropletsClient.Repository;
+
+public class DropletDataDbContext : DbContext
+{
+    public DbSet<DropletData> DropletDatas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string conn = @"";
+            optionsBuilder.UseLazyLoadingProxies()
+                .UseInMemoryDatabase(conn);
+        }
+    }
+}
