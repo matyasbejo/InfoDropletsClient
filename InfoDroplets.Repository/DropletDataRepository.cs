94452ceb95c063f7e7c3fc6129373a72d471ed41
matyasbejo <bejomatyi@gmail.com>
2024-09-24 17:41:42 +0200

    Repositories added

---
+++ InfoDroplets.Repository/DropletDataRepository.cs
@@ -0,0 +1,31 @@
+﻿using InfoDroplets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoDroplets.Repository
+{
+    public class DropletDataRepository : Repository<DropletData>, IRepository<DropletData>
+    {
+        public DropletDataRepository(ClientDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override DropletData Read(int id)
+        {
+            return ctx.DropletDataSet.FirstOrDefault(d => d.Id == id);
+        }
+
+        public override void Update(DropletData item)
+        {
+            var oldData = Read(item.Id);
+            foreach(var prop in oldData.GetType().GetProperties())
+            {
+                prop.SetValue(oldData, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ InfoDroplets.Repository/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoDroplets.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected ClientDbContext ctx;
+        public Repository(ClientDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(T item)
+        {
+            ctx.Set<T>().Remove(item);
+            ctx.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract void Update(T item);
+    }
+}
