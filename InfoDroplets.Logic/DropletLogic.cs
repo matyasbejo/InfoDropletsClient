184eab397b90f794a39863daf2ef12acd2efbd36
matyasbejo <bejomatyi@gmail.com>
2024-09-28 01:54:01 +0200

    Added logic layer for droplet & trackingentry

---
+++ InfoDroplets.Logic/DropletLogic.cs
@@ -0,0 +1,44 @@
+﻿using InfoDroplets.Models;
+using InfoDroplets.Repository;
+
+namespace InfoDroplets.Logic
+{
+    public class DropletLogic
+    {
+        IRepository<Droplet> repo;
+
+        public DropletLogic(IRepository<Droplet> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Droplet item)
+        {
+           repo.Create(item);
+        }
+
+        public void Delete(Droplet item)
+        {
+            repo.Delete(item);
+        }
+
+        public Droplet Read(int id)
+        {
+            var DataEntry = repo.Read(id);
+            if (DataEntry == null)
+                throw new ArgumentException("DataEntry doesn't exist");
+
+            return DataEntry;
+        }
+
+        public IQueryable<Droplet> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Droplet item)
+        {
+            repo.Update(item);
+        }
+    }
+}
---
+++ InfoDroplets.Logic/TrackingEntryLogic.cs
@@ -0,0 +1,52 @@
+﻿using InfoDroplets.Models;
+using InfoDroplets.Repository;
+
+namespace InfoDroplets.Logic
+{
+    public class TrackingEntryLogic
+    {
+        IRepository<TrackingEntry> repo;
+
+        public TrackingEntryLogic(IRepository<TrackingEntry> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(string data)
+        {
+            if (data.Split(";").Count() != 6)
+                throw new ArgumentException($"Input string error: {data}");
+
+            repo.Create(new TrackingEntry(data));
+        }
+
+        protected void Create(TrackingEntry item)
+        {
+           repo.Create(item);
+        }
+
+        public void Delete(TrackingEntry item)
+        {
+            repo.Delete(item);
+        }
+
+        public TrackingEntry Read(int id)
+        {
+            var DataEntry = repo.Read(id);
+            if (DataEntry == null)
+                throw new ArgumentException("DataEntry doesn't exist");
+
+            return DataEntry;
+        }
+
+        public IQueryable<TrackingEntry> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(TrackingEntry item)
+        {
+            repo.Update(item);
+        }
+    }
+}
