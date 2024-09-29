c625a4603a99a2cef6453b21ecbd62cefcb58f75
matyasbejo <bejomatyi@gmail.com>
2024-09-29 18:27:58 +0200

    new utility class

---
+++ InfoDroplets.Models/GpsPos.cs
@@ -0,0 +1,26 @@
+﻿
+namespace InfoDroplets.Models
+{
+    public class GpsPos
+    {
+        #region Properties
+        public double Latitude { get; private set; }
+
+        public double Longitude { get; private set; }
+
+        public double Elevation { get; private set; }
+        #endregion
+
+        public GpsPos(double latitude, double longitude, double elevation)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Elevation = elevation;
+        }
+
+        public override string ToString()
+        {
+            return $"{Latitude},{Longitude},{Elevation}";
+        }
+    }
+}
