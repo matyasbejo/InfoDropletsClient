746b0b89c7e37cf999c423f3283749ca7fb79e14
matyasbejo <bejomatyi@gmail.com>
2024-10-15 00:18:13 +0200

    testing code

--- InfoDroplets.ConsoleClient/Program.cs
+++ InfoDroplets.ConsoleClient/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using InfoDroplets.Logic;
 using InfoDroplets.Models;
+using InfoDroplets.Repository;
+using InfoDroplets.Utils.SerialCommunication;
 
 namespace InfoDroplets.ConsoleClient
 {
@@ -7,7 +10,34 @@ namespace InfoDroplets.ConsoleClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ClientDbContext ctx = new ClientDbContext();
+
+            DropletRepository dropletRepository = new DropletRepository(ctx);
+            TrackingEntryRepository trackingEntryRepository = new TrackingEntryRepository(ctx);
+
+            DropletLogic dropletLogic = new DropletLogic(dropletRepository);
+            TrackingEntryLogic trackingEntryLogic = new TrackingEntryLogic(trackingEntryRepository);
+
+            SerialWrapper sw = new SerialWrapper(new System.IO.Ports.SerialPort());
+
+            dropletLogic.CommandGenerated += sw.SendCommand;
+
+            var a = SerialWrapper.GetPortNames();
+            sw.SetPortName("COM4");
+            sw.SafeOpen();
+
+            dropletLogic.CommandGenerated += sw.SendCommand;
+
+            while (true)
+            {
+                var line = sw.ReadLine();
+                try
+                {
+                    trackingEntryLogic.Create(line);
+                    Console.WriteLine($"Added: {line}");
+                }
+                catch (Exception e){ Console.WriteLine(e.Message); }
+            }
         }
     }
 }
