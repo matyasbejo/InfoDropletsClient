983ac2a884679ae43ad7b5cb4c571c7a7ab78e70
matyasbejo <bejomatyi@gmail.com>
2024-11-14 01:26:31 +0100

    mainwindow basic ui layout

--- InfoDroplets.Client/MainWindow.xaml.cs
+++ InfoDroplets.Client/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using InfoDroplets.Client;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +16,32 @@ namespace InfoDropletsClient
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private List<string> _items;
+        public List<string> Items
+        {
+            get { return _items; }
+            set
+            {
+                _items = value;
+                OnPropertyChanged(nameof(Items));
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
-            SerialWindow serialSetupWindow = new SerialWindow();
-            if(serialSetupWindow.ShowDialog() == true)
-                lb_selectedPort.Content = serialSetupWindow.SelectedSerialPort;
+            DataContext = this;
+            Items = new List<string>();
+            Items.Add("1");
+            Items.Add("2");
+            Items.Add("3");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
