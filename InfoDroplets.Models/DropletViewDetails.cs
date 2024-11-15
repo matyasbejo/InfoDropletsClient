7990098e362be259c36fd9f75499e947fd1a4150
matyasbejo <bejomatyi@gmail.com>
2024-11-15 21:26:29 +0100

    Model added for easier viewmodel logic

---
+++ InfoDroplets.Models/DropletViewDetails.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using InfoDroplets.Utils.Enums;
+
+namespace InfoDroplets.Models
+{
+    public class DropletViewDetails : ObservableObject
+    {
+		public GpsPos Position { get; set; }
+		public int SatteliteCount { get; set; }
+		public double HaversineDistance { get; set; }
+		public int Speed { get; set; }
+		public DropletMovementStatus MovementStatus { get; set; }
+		public DropletVersion DropletVersion { get; set; }
+		public DateTime Time { get; set; }
+	}
+}
