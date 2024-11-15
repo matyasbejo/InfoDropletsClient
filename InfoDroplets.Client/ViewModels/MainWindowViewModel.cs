f83e984c064fcae3ad4598ff849dd28b6b564575
matyasbejo <bejomatyi@gmail.com>
2024-11-15 21:27:17 +0100

    viewmodels initialized+solution structuring

---
+++ InfoDroplets.Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using InfoDroplets.Logic;
+using InfoDroplets.Models;
+using System.Collections.ObjectModel;
+
+namespace InfoDroplets.Client.ViewModels
+{
+    internal class MainWindowViewModel : ObservableRecipient
+    {
+        DropletLogic dropletLogic;
+
+        public ObservableCollection<Droplet> DropletDetails { get; set; }
+
+
+    }
+}
