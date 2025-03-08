32178fee3b00671565e2f1335dda830ee54a8da6
matyasbejo <bejomatyi@gmail.com>
2025-03-09 00:16:19 +0100

    ResultExporter project added, base leaflet file added and modified, logic comments for future

---
+++ ResultExporter/LogEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultExporter
+{
+    internal class LogEntry
+    {
+        public LogEntry(double elevation, double latitude, double longitude)
+        {
+            Elevation = elevation;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        internal double Elevation { get; private set; }
+        internal double Latitude { get; private set; }
+        internal double Longitude { get; private set; }
+    }
+}
