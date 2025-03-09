2d09648202aacc362f80000f54a27f6861f15d53
matyasbejo <bejomatyi@gmail.com>
2025-03-10 00:01:28 +0100

    Added logic to process single file and filter a/b files

--- ResultExporter/LogFileProcessor.cs
+++ ResultExporter/LogFileProcessor.cs
@@ -11,7 +11,7 @@ namespace InfoDroplets.ResultExporter
 {
     internal static class LogFileProcessor
     {
-        internal static List<LogEntry> GetFileData(string path)
+        static string[] GetDataRowsFromFile(string path)
         {
             if(!File.Exists(path)) throw new FileNotFoundException(path);
 
@@ -21,21 +21,57 @@ namespace InfoDroplets.ResultExporter
 
             int IndexOfSeparator = Array.IndexOf(FileLines, "________;________;________;________;________;________");
 
-            var fileData = FileLines.Skip(IndexOfSeparator+1).ToList();
+            string[] fileData = FileLines.Skip(IndexOfSeparator+1).ToArray();
+            return fileData;
+        }
 
-            List<LogEntry> result = new List<LogEntry>();
+        static List<LogEntry> ProcessFile(string path)
+        {
+            var fileData = GetDataRowsFromFile(path);
+
+            List<LogEntry> convertedValues = new List<LogEntry>();
             foreach (string row in fileData)
             {
                 var rowData = row.Split(';');
                 if (rowData.Count() != 6) throw new Exception($"The line {row} is not a valid LogEntry.");
-                
+
                 var Lat = double.Parse(rowData[3]);
                 var Long = double.Parse(rowData[4]);
                 var El = double.Parse(rowData[5]);
 
-                result.Add(new LogEntry(Lat, Long, El));
+                convertedValues.Add(new LogEntry(Lat, Long, El));
             }
+
+            List<LogEntry> result = convertedValues.Where(le => le.Latitude != 0 && le.Longitude != 0).Distinct().ToList();
             return result;
+        }
+
+        static string[] FilterABFiles(ref string[] paths)
+        {
+            var aFiles = paths.Where(p => p.Replace(".txt", "").Last() == 'a').ToArray();
+            var bFiles = paths.Where(p => p.Replace(".txt", "").Last() == 'b').ToArray();
+
+            if (aFiles.Length == 0)
+            {
+                paths = bFiles;
+            }
+            else if (bFiles.Length == 0)
+            {
+                paths = aFiles;
+            }
+            else
+            {
+                FileInfo aFileInfo = new FileInfo(aFiles[0]);
+                FileInfo bFileInfo = new FileInfo(bFiles[0]);
+                if (aFileInfo.Length == 0)
+                    paths = bFiles;
+                else if (bFileInfo.Length == 0)
+                    paths = aFiles;
+                else paths = aFiles;
+            }
+
+            paths = paths.OrderBy(p => p).ToArray();
+            return paths;
         }
     }
 }
