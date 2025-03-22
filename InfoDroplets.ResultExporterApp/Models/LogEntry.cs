1bc148bd9a74478ac187b4e82519486f6ba24549
matyasbejo <bejomatyi@gmail.com>
2025-03-23 00:56:13 +0100

    Changed project to wpf from console

---
+++ InfoDroplets.ResultExporterApp/Models/LogEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoDroplets.ResultExporter.Models
+{
+    internal class LogEntry
+    {
+        public LogEntry(double latitude, double longitude, double elevation)
+        {
+            Elevation = elevation;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        internal double Elevation { get; private set; }
+        internal double Latitude { get; private set; }
+        internal double Longitude { get; private set; }
+
+        public override string ToString()
+        {
+            return $"[{Math.Round(Latitude,6)}, {Math.Round(Longitude, 6)}, {Math.Round(Elevation, 1)}]";
+        }
+    }
+}
---
+++ InfoDroplets.ResultExporterApp/View/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using ResultExporterApp;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InfoDroplets.ResultExporterApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void teszt_Click(object sender, RoutedEventArgs e)
+        {
+            string[] dummypaths =
+            {
+                @"D:\UNI\_Szakdolgozat\TestData\L8_V0a.txt",
+                @"D:\UNI\_Szakdolgozat\TestData\L8_V0b.txt",
+                @"D:\UNI\_Szakdolgozat\TestData\L8_V1a.txt",
+                @"D:\UNI\_Szakdolgozat\TestData\L8_V1b.txt",
+                @"D:\UNI\_Szakdolgozat\TestData\L8_V2a.txt",
+                @"D:\UNI\_Szakdolgozat\TestData\L8_V2b.txt"
+            };
+
+            LogProcessor.Execute(dummypaths);
+            MapGenerator.CreateMap(LogProcessor.DropletNumber, LogProcessor.ElevationRange, LogProcessor.CenterPos.Longitude, LogProcessor.CenterPos.Latitude, LogProcessor.GlobalLogCollection, LogProcessor.GlobalBreakCollection);
+        }
+    }
+}
